26930e3a96e2d54519bc12ad8116c2f744f5bea1
Furjes-Benke Peter <fupn26@hotmail.hu>
2020-09-23 23:26:18 +0200

    added basic tokenizing and token displaying to script.js

--- sources/script.js
+++ sources/script.js
@@ -1,12 +1,43 @@
-const currStyle = document.getElementById("current-style")
-const styleSelect = document.querySelector('[pick-style]')
-
-styleSelect.addEventListener('change', select => {
-  if(styleSelect.value == 'normal'){
-    currStyle.href="style_base.css"
-  }
+$(document).ready(() => {
+  var tokens = [];
+  const currStyle = document.getElementById("current-style")
+  const styleSelect = document.querySelector('[pick-style]')
   
-  if(styleSelect.value == 'dark'){
-    currStyle.href="style_dark.css"
+  styleSelect.addEventListener('change', select => {
+    if(styleSelect.value == 'normal'){
+      currStyle.href="style_base.css"
+    }
+    
+    if(styleSelect.value == 'dark'){
+      currStyle.href="style_dark.css"
+    }
+  });
+
+  $(".button").click(function (event) {
+      processButtonPress(event.target);
+  });
+
+  function processButtonPress(button) {
+      var value = $(button).val();
+      if (!isNaN(value)) {
+          var prevToken = tokens[tokens.length - 1];
+          if (!isNaN(prevToken)) {
+              tokens[tokens.length - 1] = prevToken + value;
+          } else {
+              tokens.push(value);
+          }
+      } else {
+          tokens.push(value);
+      }
+
+      displayExpression();
   }
-})
+
+  function displayExpression() {
+      var outputString = "";
+      tokens.forEach(element => {
+          outputString += element;
+      });
+      $("#output").html(outputString);
+  }
+});
